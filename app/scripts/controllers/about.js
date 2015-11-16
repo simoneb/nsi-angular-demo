ff5ef3a73d925708e3105541c9299265cf1fe235
Simone Busoli <simone.busoli@gmail.com>
2015-11-16 13:18:29 +0100

    initial import

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularYoDemoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularYoDemoApp
+ */
+angular.module('angularYoDemoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularYoDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularYoDemoApp
+ */
+angular.module('angularYoDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
